09e79efe2929677b5c10e436f3572f6205b08fbb
Kazys Sketrys <kazys.sketrys@triad.co.uk>
2017-04-04 16:47:06 +0100

    Changes triggered by auth-checker-lig

--- api/src/test/java/uk/gov/hmcts/reference/api/componenttests/ComponentTestConfiguration.java
+++ api/src/test/java/uk/gov/hmcts/reference/api/componenttests/ComponentTestConfiguration.java
@@ -3,7 +3,7 @@ package uk.gov.hmcts.reference.api.componenttests;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import uk.gov.hmcts.auth.checker.SubjectResolver;
-import uk.gov.hmcts.auth.checker.User;
+import uk.gov.hmcts.auth.checker.user.User;
 import uk.gov.hmcts.auth.provider.service.token.ServiceTokenGenerator;
 import uk.gov.hmcts.reference.api.componenttests.backdoors.UserResolverBackdoor;
 
